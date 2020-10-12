6000b219db49a514a530193450ff3da31981dd0f
Fran Gonzalez <Fran Gonzalez@FRANGE>
2020-10-11 21:58:15 -0300

    Practico 8 HashSet

---
+++ src/practico8/Colegio.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico8;
+
+/**
+ *
+ * @author Fran Gonzalez
+ */
+public class Colegio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Materia matematica = new Materia(101, "Matematica", 1);
+        Materia ingles = new Materia(102, "Ingles I", 1);
+        Materia lab1 = new Materia(103, "Laboratorio I", 1);
+        
+        Alumno lopmar = new Alumno(1001, "Lopez", "Martin");
+        Alumno martbren = new Alumno(1002, "Martinez", "Brenda");
+        
+        
+        System.out.println("Agregando materias para el alumno: " + lopmar.getApellido() + lopmar.getNombre());
+        lopmar.agregarMateria(lab1);
+        lopmar.agregarMateria(matematica);
+        lopmar.agregarMateria(ingles);
+        
+        System.out.println("Agregando materias para el alumno: " + martbren.getApellido() + martbren.getNombre());
+        martbren.agregarMateria(lab1);
+        martbren.agregarMateria(lab1);
+        martbren.agregarMateria(matematica);
+        martbren.agregarMateria(ingles);
+        
+        
+        
+    }
+    
+}
