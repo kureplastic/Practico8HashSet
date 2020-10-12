d30dd9d7ccbd5ff35e1129b3a9546f3ccf3a71cc
Fran Gonzalez <Fran Gonzalez@FRANGE>
2020-10-11 22:02:22 -0300

    Practico 8 HashSet

---
+++ src/practico8/Materia.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico8;
+
+/**
+ *
+ * @author Fran Gonzalez
+ */
+public class Materia {
+    
+    private int idMateria;
+    private String nombre;
+    private int anio;
+
+    public Materia(int idMateria, String nombre, int anio) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+    
+    
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Materia) && (obj == null))
+            return false;
+        else
+            return (this.idMateria == ((Materia)obj).idMateria);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
